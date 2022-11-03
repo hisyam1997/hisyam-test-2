8941968fe26eb455a6d677d039077b52aa5a3997
Muhammad Hisyam <muhammad16026@mail.unpad.ac.id>
2022-11-03 16:55:55 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, alias totam similique eum delectus sapiente quo necessitatibus eos dolor quae consequuntur adipisci quas harum officia repellendus dolorum obcaecati esse possimus?</p>
 </body>
 </html>
